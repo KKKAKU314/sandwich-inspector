b30c5c4397a214aa21b70c98050e1da72759f17e
jia314 <guojiadla@gmail.com>
2025-01-09 19:03:29 +0900

    Initial commit

---
+++ src/extractRealSandwich.js
@@ -0,0 +1,166 @@
+import { ethers } from 'ethers'; 
+import dotenv from 'dotenv';
+dotenv.config({ path: '../config/.env' });
+
+const nodeAddress = process.env.NODE_ADDRESS;
+const provider = new ethers.JsonRpcProvider(nodeAddress);
+const transferEventSignature = "0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef";
+
+const ABI_name = ["function name() view returns (string)"];
+const ABI_Decimals = ["function decimals() view returns (uint8)"];
+
+
+export async function extractRealSandwich(map,blockNumber) {
+    const txDetailArr = [];
+    const txHashArr = Array.from(map.keys()); 
+    const swapPoolArr = Array.from(map.values());
+
+    const frontRunTxReceipt = await provider.getTransactionReceipt(txHashArr[0]);
+    const backRunTxReceipt = await provider.getTransactionReceipt(txHashArr[txHashArr.length - 1]);
+
+    const frontRunTxContent = await decodeLogs(frontRunTxReceipt);
+    const frontRunSwapPoolArr = swapPoolArr[0];
+    const frontRunSwapSequenceMap = getSwapSequenceMap(frontRunSwapPoolArr, frontRunTxContent);
+    const frontRunTxFee = ethers.formatUnits(frontRunTxReceipt.gasPrice * frontRunTxReceipt.gasUsed, "ether");
+    const frontRunTokens = getAssociatedTokens(frontRunSwapSequenceMap);
+
+    const backRunTxContent = await decodeLogs(backRunTxReceipt);
+    const backRunSwapPoolArr = swapPoolArr[swapPoolArr.length - 1];
+    const backRunSwapSequenceMap = getSwapSequenceMap(backRunSwapPoolArr, backRunTxContent);
+    const backRunTxFee = ethers.formatUnits(backRunTxReceipt.gasPrice * backRunTxReceipt.gasUsed, "ether");
+    const backRunTokens = getAssociatedTokens(backRunSwapSequenceMap);
+    
+    const frontRunDetailArr = [blockNumber,'Front run', txHashArr[0], frontRunTokens, swapPoolArr[0], frontRunTxReceipt.gasPrice, frontRunTxFee, frontRunTxReceipt.index];
+    const backRunDetailArr = [blockNumber,'Back run', txHashArr[txHashArr.length - 1], backRunTokens, swapPoolArr[swapPoolArr.length - 1], backRunTxReceipt.gasPrice, backRunTxFee, backRunTxReceipt.index];
+    txDetailArr.push(frontRunDetailArr);
+
+    if(frontRunTxReceipt.from === backRunTxReceipt.from ||
+       frontRunTxReceipt.to === backRunTxReceipt.to){
+        let isSandwich = true;
+    // loop victim txs
+    for (let i = 1; i < txHashArr.length - 1; i++) {
+        const victimTxReceipt = await provider.getTransactionReceipt(txHashArr[i]);
+        const victimTxContent = await decodeLogs(victimTxReceipt);
+        const victimSwapPoolArr = swapPoolArr[i];
+        const victimSwapSequenceMap = getSwapSequenceMap(victimSwapPoolArr, victimTxContent);
+        const backRunTxFee = ethers.formatUnits(victimTxReceipt.gasPrice * victimTxReceipt.gasUsed, "ether");
+        const victimTokens = getAssociatedTokens(victimSwapSequenceMap);
+
+        const victimDetailArr = [blockNumber,`victim${i}`,txHashArr[i], victimTokens, swapPoolArr[i], victimTxReceipt.gasPrice, backRunTxFee, victimTxReceipt.index];
+        txDetailArr.push(victimDetailArr);
+
+        if (!SameOrReverseSwapSequence(victimSwapSequenceMap, frontRunSwapSequenceMap) ||
+            SameOrReverseSwapSequence(victimSwapSequenceMap, backRunSwapSequenceMap)) {
+            isSandwich = false;
+            break;
+        }
+    }
+    if (isSandwich) {
+        txDetailArr.push(backRunDetailArr);
+        // console.log(txDetailArr);
+
+        return txDetailArr;
+    }
+    }
+    return null;
+}
+
+
+function getAssociatedTokens(swapSequenceMap){
+    const uniqueTokens = new Set();
+    for (const swapPoolArr of swapSequenceMap.values()) {
+        for (const swap of swapPoolArr) {
+            const tokenName = swap[1]; 
+            uniqueTokens.add(tokenName); 
+        }
+    }
+    return Array.from(uniqueTokens); 
+}
+
+
+function getSwapSequenceMap(swapPoolArr, transferContentArr) {
+    const resultMap = new Map();
+    for (const poolAddress of swapPoolArr) {
+        resultMap.set(poolAddress.toLowerCase(), []);
+    }
+    for(const transferContent of transferContentArr){
+        const fromAddress = transferContent[2].toLowerCase();
+        const toAddress = transferContent[3].toLowerCase();
+        if (resultMap.has(fromAddress) || resultMap.has(toAddress)) {
+            if (resultMap.has(fromAddress)) {
+                resultMap.get(fromAddress).push(transferContent);
+            }
+            if (resultMap.has(toAddress)) {
+                resultMap.get(toAddress).push(transferContent);
+            }
+        }
+    }
+    return resultMap;
+}
+
+
+//compare victim with fornt/back run
+function SameOrReverseSwapSequence(victimSwapSequenceMap, frontBackRunSwapSequenceMap) {
+    let result = false;
+    for (const [victimPool, victimSwapFlow] of victimSwapSequenceMap) {
+        if (!frontBackRunSwapSequenceMap.has(victimPool)) continue;
+        const frontBackRunSwapFlow =  frontBackRunSwapSequenceMap.get(victimPool);
+        if(frontBackRunSwapFlow[0][1] === victimSwapFlow[0][1] &&
+           frontBackRunSwapFlow[1][1] === victimSwapFlow[1][1] ){
+            //same
+            result = true;
+        }
+        if(frontBackRunSwapFlow[0][1] === victimSwapFlow[1][1] &&
+           frontBackRunSwapFlow[1][1] === victimSwapFlow[0][1]){
+            //reverse
+            result = false;
+         }
+    }
+    return result;
+}
+
+
+async function decodeLogs(receipt) {
+    const result = [];
+    for (let i = 0; i < receipt.logs.length; i++) {
+        const currentLog = receipt.logs[i];
+        const tokenName = await getTokenName(currentLog.address);
+        // get Transfer Content
+        if (currentLog.topics[0] === transferEventSignature) {
+            const tokenAmountRaw = ethers.toBigInt(currentLog.data);
+            const tokenDecimal = await getTokenDecimal(currentLog.address);
+            const topicAddress1 = "0x" + currentLog.topics[1].slice(-40);
+            const topicAddress2 = "0x" + currentLog.topics[2].slice(-40);
+
+            result.push([
+                ethers.formatUnits(tokenAmountRaw, tokenDecimal),
+                tokenName,
+                topicAddress1,
+                topicAddress2
+            ]);
+        }
+    }
+    return result;
+}
+
+
+async function getTokenName(address) {
+    try {
+        const tokenContract = new ethers.Contract(address, ABI_name, provider);
+        return await tokenContract.name();
+    } catch (error) {
+        return "Unknown Token";
+    }
+}
+
+async function getTokenDecimal(address) {
+    try {
+        const tokenContract = new ethers.Contract(address, ABI_Decimals, provider);
+        return await tokenContract.decimals();
+    } catch (error) {
+        return 18;
+    }
+}
+
+
+
---
+++ src/main1.js
@@ -0,0 +1,188 @@
+import { ethers } from 'ethers';
+import { extractRealSandwich } from './extractRealSandwich.js'
+import dotenv from 'dotenv';
+dotenv.config({ path: '../config/.env' });
+
+const nodeAddress = process.env.NODE_ADDRESS;
+const provider = new ethers.JsonRpcProvider(nodeAddress);
+const V2SwapEventSignature = "0xd78ad95fa46c994b6551d0da85fc275fe613ce37657fb8d5e3d130840159d822";
+const V3SwapEventSignature = "0xc42079f94a6350d7e6235f29174924f928cc2ac818eb64fed8004e115fbcca67";
+
+const blockFrom = parseInt(process.env.BLOCK_FROM, 10);
+const blockTo = parseInt(process.env.BLOCK_TO, 10);
+
+
+function findConsecutiveSwaps(map) {
+    const result = [];
+    let keys = Array.from(map.keys());
+    let temp = [keys[0]];
+    for (let i = 1; i < keys.length; i++) {
+        if (keys[i] === keys[i - 1] + 1) {
+            temp.push(keys[i]);
+        } else {
+            if (temp.length >= 3) {
+                result.push(temp.join(', '));
+            }
+            temp = [keys[i]];
+        }
+    }
+    if (temp.length >= 3) {
+        result.push(temp.join(', '));
+    }
+    return result;
+}
+
+
+function findPossibleSandwich(arr) {
+    const result = [];
+    arr.forEach(map => {
+        const values = Array.from(map.values());
+        if (map.size === 3) {
+            if (checkSameContent(values[0], values[2]) === true &&
+                checkIntersection(values[0], values[1]) === true) {
+                result.push(map);
+            }
+        } else {
+            const matchingMaps = findMatchingContent(map);
+            if (matchingMaps.length > 0) {
+                result.push(...matchingMaps);
+            }
+        }
+    });
+    return result;
+}
+
+
+function checkSameContent(arr1, arr2) {
+    if (arr1.length !== arr2.length) {
+        return false;
+    }
+    const sorted1 = [...arr1].sort();
+    const sorted2 = [...arr2].sort();
+    for (let i = 0; i < sorted1.length; i++) {
+        if (sorted1[i] !== sorted2[i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+
+function checkIntersection(arr1, arr2) {
+    const set = new Set(arr1);
+    for (const item of arr2) {
+        if (set.has(item)) {
+            return true;
+        }
+    }
+    return false;
+}
+
+
+function findMatchingContent(map) {
+    const arr = Array.from(map.values());
+    const keys = Array.from(map.keys());
+    const results = [];
+
+    for (let i = 0; i < arr.length - 2; i++) {
+        const first = arr[i];
+        for (let j = i + 2; j < arr.length; j++) {
+            const last = arr[j];
+            if (checkSameContent(first, last)) {
+                let valid = true;
+                const intermediate = [];
+                for (let k = i + 1; k < j; k++) {
+                    if (checkIntersection(first, arr[k])) {
+                        intermediate.push(arr[k]);
+                    } else {
+                        valid = false;
+                        break;
+                    }
+                }
+                if (valid) {
+                    const newMap = new Map();
+                    newMap.set(keys[i], first);
+                    intermediate.forEach((value, index) => {
+                        newMap.set(keys[i + 1 + index], value);
+                    });
+                    newMap.set(keys[j], last);
+                    results.push(newMap);
+                }
+            }
+        }
+    }
+    return results;
+}
+
+
+//main function
+async function main() {
+    let countSandwich = 0;
+    for (let blockCurrent = blockFrom; blockCurrent <= blockTo; blockCurrent++) {
+        let sandwichIndex = 0;
+        try {
+            const block = await provider.getBlock(blockCurrent);
+            console.log(`Block ${blockCurrent}:`);
+            // Store all transactions contain swap
+            const swapTxsMap = new Map();
+  
+            for (let txHash of block.transactions) {
+                const receipt = await provider.getTransactionReceipt(txHash);
+                for (let i = 0; i < receipt.logs.length; i++) {
+                    let log = receipt.logs[i];
+                    if (
+                        log?.topics[0] === V2SwapEventSignature ||
+                        log?.topics[0] === V3SwapEventSignature
+                    ) {
+                        if (!swapTxsMap.has(receipt.index)) {
+                            swapTxsMap.set(receipt.index, []);
+                        }
+                        swapTxsMap.get(receipt.index).push(log.address);
+                    }
+                }
+                if (swapTxsMap.has(receipt.index)) {
+                    const poolAddresses = swapTxsMap.get(receipt.index);
+                    swapTxsMap.set(receipt.index, [poolAddresses, txHash]);
+                }
+            }
+            const consectiveSwaps = findConsecutiveSwaps(swapTxsMap);
+            const resultMaps = [];
+            for (let item of consectiveSwaps) {
+                const map = new Map();
+                const numbers = item.split(",").map(num => num.trim());
+                //change map’s structure
+                for (let n of numbers) {
+                    const key = swapTxsMap.get(Number(n))[1]; // Use txHash as key
+                    const value = swapTxsMap.get(Number(n))[0]; // Use poolAddresses as value
+                    map.set(key, value);
+                }
+                resultMaps.push(map);
+            }
+            const possibleSandwiches = findPossibleSandwich(resultMaps);
+            
+            for (let i = 0; i < possibleSandwiches.length; i++) {
+
+                const resultArr = await extractRealSandwich(possibleSandwiches[i], blockCurrent);
+                if (resultArr && resultArr.length > 0) {
+                    sandwichIndex++;  
+                }
+                console.log(resultArr);
+                
+            }
+        
+            console.log(`Block ${blockCurrent} found ${sandwichIndex} sandwich attacks.`);
+            countSandwich = countSandwich + sandwichIndex;
+
+        } catch (error) {
+            console.error(`Error fetching block ${blockCurrent}:`, error);
+        }
+    }
+    console.log(`Found ${countSandwich} Sandwich Attacks between block ${blockFrom} and block ${blockTo}`);
+
+}
+
+
+//execute
+main();
+
+
